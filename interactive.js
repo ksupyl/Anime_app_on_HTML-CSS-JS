e441c0d9339353bd9d6d815ff3d339650ecce4cc
Ksenia <Xeonica01owl@gmail.com>
2024-07-03 19:11:51 +0300

    Commit 2 - final project - add .css and .js with API

---
+++ interactive.js
@@ -0,0 +1,34 @@
+async function searchAnime() {
+    const query = document.getElementById('searchInput').value;
+    const response = await fetch(
+        `https://kitsu.io/api/edge/anime?filter[text]=${query}`
+    );
+    const data = await response.json();
+    displayAnime(data.data);
+}
+
+function displayAnime(animeList) {
+    const animeListContainer = document.getElementById("animeList");
+    animeListContainer.innerHTML = "";
+
+    animeList.forEach((anime) => {
+        const animeItem = document.createElement("div");
+        animeItem.classList.add("anime-item");
+
+        const title = anime.attributes.canonicalTitle;
+        const synopsis = anime.attributes.synopsis;
+        const rating = anime.attributes.ageRating;
+        const episodeCount = anime.attributes.episodeCount;
+        const posterImage = anime.attributes.posterImage.original;
+
+        animeItem.innerHTML = `
+        <img src="${posterImage}" alt="${title}">
+        <h3>${title}</h3>
+        <p>${synopsis}</p>
+        <span>Rating: ${rating}</span>
+        <span>Episodes: ${episodeCount}</span>
+    `;
+
+        animeListContainer.appendChild(animeItem);
+    });
+}
